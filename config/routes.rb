b4b9003995295920b041e45c020d7f5c395a55a4
Sahil Shekhawat <sahilshekhawat01@gmail.com>
2014-11-05 23:40:09 +0530

    created static pages controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/help'
+
   resources :users
   root to: 'visitors#index'
   get '/auth/google_oauth2/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
   get '/auth/failure' => 'sessions#failure'
+  get '/about' => 'static_pages#about'
+  get '/help' => 'static_pages#help'
+
 end
